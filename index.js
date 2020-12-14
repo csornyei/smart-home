930a8a29425b93ccdec9bfcac6c409aecbebc9bc
Mate Csornyei <mate@csornyei.com>
2020-12-14 20:33:33 +0100

    basic views

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/', mainRoutes);
 
 app.use('*', (req, res) => {
     res.status(404);
-    res.send('Page not found')
+    res.render('notfound')
 })
 
 const PORT = process.env.PORT || 8888;
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {active: 'home'})
+});
+
+router.get('/statistics', (req, res) => {
+    console.log('hello')
+    res.render('stats', {active: 'stats'})
 });
 
 module.exports = router;
